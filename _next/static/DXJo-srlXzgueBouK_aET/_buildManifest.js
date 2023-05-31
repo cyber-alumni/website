989297d4d035b0e2dfcabf64111b58e8a182e0a5
matandobr <matandobr@users.noreply.github.com>
2023-05-31 19:32:38 +0000

    deploy: 105b7f16a8d1dfeb4b80eb93df9948bc7e55aa2e

--- _next/static/DXJo-srlXzgueBouK_aET/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/efd308111ef03793.css","static/chunks/pages/index-ad8a885b0841426b.js"],"/_error":["static/chunks/pages/_error-3986dd5834f581dc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
